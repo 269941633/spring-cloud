eb1a530fbeb117c440962655bed994d77666a85d
Jianfei Wei <269941633@qq.com>
2017-08-03 17:40:12 +0800

    添加对hystrix dashboard的支持

--- eureka/eureka-app/eureka-app-ribbo-hystrix/src/main/java/com/fei/springcloud/Application.java
+++ eureka/eureka-app/eureka-app-ribbo-hystrix/src/main/java/com/fei/springcloud/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient 
 @SpringBootApplication
 @EnableHystrix    //增加hystrix断路器
+@EnableHystrixDashboard  //支持hystrix dashboard
 public class Application {
 
 	@Bean //定义REST客户端，RestTemplate实例
