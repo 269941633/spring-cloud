25788f7b8c635c70be5f15a2344eaa08508aa58a
Jianfei Wei <269941633@qq.com>
2017-08-09 12:23:05 +0800

    api服务网关zuul,反向代理

---
+++ zuul/src/main/java/com/fei/springcloud/Application.java
@@ -0,0 +1,15 @@
+package com.fei.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy   
+@SpringCloudApplication 
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+}
